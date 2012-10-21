8b96073f04ea9d13f3452dcc909e1181da79f7b8
Dmitry Motylev <dmotylev@myba2.local>
2012-10-21 15:37:16 +0400

    colorize ant output

--- bash/bash.d/50_ant.sh
+++ bash/bash.d/50_ant.sh
@@ -1,2 +1,3 @@
 export ANT_HOME=/usr/local/share/ant
-export ANT_OPTS="-Dfile.encoding=UTF-8"
+export ANT_ARGS='-logger org.apache.tools.ant.listener.AnsiColorLogger'
+export ANT_OPTS="-Dfile.encoding=UTF-8 -Dant.logger.defaults=$HOME/.dotfiles/share/ant/logger_defaults.properties"

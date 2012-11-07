938d732174590ed6236b36361408161a5e5d7947
Dmitry Motylev <dmotylev@gmail.com>
2012-11-07 16:17:20 +0400

    cleared prompt

--- bash/bash.d/10_prompt.sh
+++ bash/bash.d/10_prompt.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 _psc_rst=$(tput sgr0)
-_psc_sbrkt=$(tput setaf 3)
-_psc_path=$(tput setaf 8)
-_psc_git_dirty=$(tput setaf 196)
-_psc_git_ready=$(tput setaf 226)
+_psc_path=$(tput setaf 238)
+_psc_git_dirty=$(tput setaf 1)
+_psc_git_ready=$(tput setaf 130)
 _psc_git_clean=$(tput setaf 4)
 _psc_prompt=$(tput setaf 6)
-_psc_history_no=$(tput setaf 8)
+_psc_history_no=$(tput setaf 240)
 _psc_smiley=$(tput setaf 4)
 _psc_frowny=$(tput setaf 1)
 
@@ -26,7 +25,7 @@ __psc_git_branch()
   else
     _git_color=$_psc_git_dirty
   fi
-  printf " $_git_color$_git_branch"
+  printf " $_git_color($_git_branch)"
 }
 
 function __psc_precmd_hook {
@@ -34,26 +33,32 @@ function __psc_precmd_hook {
 }
 
 function __psc_postcmd_hook {
-  if [ $? -eq 0 ]; then
+  _cmd_code=$?
+  if [ $_cmd_code -eq 0 ]; then
     _cmd_color=$_psc_smiley
-    _cmd_char="✔ "
+    _cmd_char=""
   else
     _cmd_color=$_psc_frowny
-    _cmd_char="✘ "
+    _cmd_char="$_cmd_code"
   fi
   _cmd_time=$(($SECONDS - $timer))
   if [ $_cmd_time -eq 0 ]; then
     _cmd_times=""
   else
-    _cmd_times="${_cmd_time}s "
+    _cmd_times="$(test -z ${_cmd_char} || echo ":")${_cmd_time}s"
   fi
-  __psc_last_cmd_result=$(printf "${_cmd_color}${_cmd_char}${_cmd_times}")
-  unset timer _cmd_times _cmd_time _cmd_color _cmd_char
+  _cmd_result="${_cmd_char}${_cmd_times}"
+  if [ -z $_cmd_result ]; then
+    __psc_last_cmd_result=""
+  else
+    __psc_last_cmd_result=$(printf "${_cmd_color}(${_cmd_result}) ")
+  fi
+  unset timer _cmd_times _cmd_time _cmd_color _cmd_char _cmd_code _cmd_result
 }
 
 trap '__psc_precmd_hook' DEBUG
 PROMPT_COMMAND=__psc_postcmd_hook
-
-export PS1='\[${_psc_sbrkt}\][\[${__psc_last_cmd_result}${_psc_path}\]\w$(__psc_git_branch) \[$_psc_history_no\]\!\[$_psc_sbrkt\]]\n\[$_psc_prompt\]\$\[$_psc_rst\] '
+#- 3s:6814:~/Workshop/go/library \[${_psc_cmd_id}\]  
+export PS1='\[${__psc_last_cmd_result}${_psc_path}\]\[${_psc_history_no}\!\] \[${_psc_path}\w\]$(__psc_git_branch)\n\[$_psc_prompt\]\$\[$_psc_rst\] '
 export PS2='> '
 export PS4='+ '

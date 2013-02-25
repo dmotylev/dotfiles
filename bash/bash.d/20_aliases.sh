df1ef41b60ea3b0b502a9c0d3e6e1ed994c22b16
Dmitry Motylev <dmotylev@gmail.com>
2013-02-25 12:19:55 +0400

    added aliases

--- bash/bash.d/20_aliases.sh
+++ bash/bash.d/20_aliases.sh
@@ -4,3 +4,4 @@ alias -- -="cd -"
 alias g='egrep'
 alias glacier='java -jar ~/bin/glacier-uploader-jar-with-dependencies.jar'
 alias gam='python /usr/local/gam/gam.py'
+alias ldapsearch='ldapsearch -y ~/.ldapsearch -H ldaps://ldap.2reallife.net:636 -D "uid=dmotylev,ou=Users,dc=2reallife,dc=net"'

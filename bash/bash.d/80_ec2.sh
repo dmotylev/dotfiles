62f49972000338526711ec8d14b12a83fa226dd6
Dmitry Motylev <dmotylev@gmail.com>
2013-12-13 23:03:09 +0000

    improve aws support

--- bash/bash.d/80_ec2.sh
+++ bash/bash.d/80_ec2.sh
@@ -1,4 +1,7 @@
 export JAVA_HOME="$(/usr/libexec/java_home)"
 export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ec2/pk-*.pem | /usr/bin/head -1)"
 export EC2_CERT="$(/bin/ls "$HOME"/.ec2/cert-*.pem | /usr/bin/head -1)"
-export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.6.12.0/libexec"
+export EC2_HOME="$(brew --prefix ec2-api-tools)"
+if [ -f ~/.ec2/env ]; then
+	. ~/.ec2/env
+fi

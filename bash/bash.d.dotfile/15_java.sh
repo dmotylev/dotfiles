20e62d8aa24278f9761fc74c43b0fa8428e411df
Dmitry Motylev <dmotylev@gmail.com>
2014-10-25 16:34:29 +0100

    add TODO function to profile and some to do to work on later

--- bash/bash.d.dotfile/15_java.sh
+++ bash/bash.d.dotfile/15_java.sh
@@ -3,10 +3,10 @@ case "$(uname -s)" in
     if [ $(/usr/libexec/java_home -F 2> /dev/null) ]; then
         export JAVA_HOME="$(/usr/libexec/java_home)"
     else
-        printf "$(tput setaf 1)TODO$(tput sgr0) Install JVM with '/usr/libexec/java_home --request'\n"
+        TODO "Install JVM with '/usr/libexec/java_home --request'\n"
     fi
     ;;
 * )
-    printf "$(tput setaf 1)TODO$(tput sgr0) Set JAVA_HOME in '~/.bash.d/15_java.sh'\n"
+    TODO "Set JAVA_HOME in '~/.bash.d/15_java.sh'\n"
     ;;
 esac
--- bash/bash.d.dotfile/70_ls.sh
+++ bash/bash.d.dotfile/70_ls.sh
@@ -4,3 +4,4 @@ alias l='ls -p'
 export CLICOLOR=true
 export LSCOLORS=hxAxdxdxcxDeHeBxbxHcHb
 export GREP_OPTIONS="--color=auto"
+TODO "Fix ~/.bash.d/70_ls.sh"

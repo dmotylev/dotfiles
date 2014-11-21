045f9d197e6875c4e3c72bdbabdac134301e5401
Dmitry Motylev <dmotylev@gmail.com>
2014-11-21 19:51:39 +0000

    gcloud and dir studio

---
+++ bash/bash.d.dotfile/35_gcloud.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# The next line updates PATH for the Google Cloud SDK.
+source '/usr/local/google-cloud-sdk/path.bash.inc'
+# The next line enables bash completion for gcloud.
+source '/usr/local/google-cloud-sdk/completion.bash.inc'

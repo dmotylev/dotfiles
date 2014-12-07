ae0a7e5af59eb8a3fa990d06b8a765dab3904afe
Dmitry Motylev <dmotylev@gmail.com>
2014-12-07 19:31:01 +0000

    some fixes

--- bash/bash.d.dotfile/30_golang.sh
+++ bash/bash.d.dotfile/30_golang.sh
@@ -1,2 +1,3 @@
 export GOPATH=${HOME}/Workshop/go
-PATH=${PATH}:$(brew --prefix go)/libexec/bin:${GOPATH}/bin
+#PATH=${PATH}:$(brew --prefix go)/libexec/bin:${GOPATH}/bin
+PATH=${PATH}:/usr/local/go/bin:${GOPATH}/bin

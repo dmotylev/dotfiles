3eb7675270793742e1eacafb4181f3eb5b9dd8eb
Dmitry Motylev <dmotylev@gmail.com>
2015-09-22 16:10:47 +0100

    tmux tweaks

--- bash/bash.d.dotfile/99_bash_completion.sh
+++ bash/bash.d.dotfile/99_bash_completion.sh
@@ -2,3 +2,6 @@ brew_prefix=$(brew --prefix)
 if [ $? -eq 0 -a -f ${brew_prefix}/etc/bash_completion ]; then
 	. ${brew_prefix}/etc/bash_completion
 fi
+if [ -f ${brew_prefix}/share/bash-completion/bash_completion ]; then
+	. ${brew_prefix}/share/bash-completion/bash_completion
+fi

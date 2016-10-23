7e3893c1ca1a4f795b460b68711fc40cfa477df4
Dmitry Motylev <dmotylev@gmail.com>
2016-10-23 19:11:02 +0100

    new terminal session is under third of second

--- bash/bash.d.dotfile/70_ls.sh
+++ bash/bash.d.dotfile/70_ls.sh
@@ -1,4 +1,4 @@
-[ $(type -p dircolors) ] || alias dircolors="$(brew --prefix coreutils)/libexec/gnubin/dircolors"
+[ $(type -p dircolors) ] || alias dircolors="$(brew--prefix coreutils)/libexec/gnubin/dircolors"
 eval $(dircolors $DOTFILES_ROOT/bash/dircolors.256dark)
 alias ls='ls --color=auto'
 alias ll='ls -ltrhp'
--- bash/bash.d.dotfile/99_bash_completion.sh
+++ bash/bash.d.dotfile/99_bash_completion.sh
@@ -1,9 +1,10 @@
-brew_prefix=$(brew --prefix)
+brew_prefix=$(brew--prefix)
 if [ $? -eq 0 -a -f ${brew_prefix}/etc/bash_completion ]; then
 	. ${brew_prefix}/etc/bash_completion
 fi
 if [ -f ${brew_prefix}/share/bash-completion/bash_completion ]; then
 	. ${brew_prefix}/share/bash-completion/bash_completion
 fi
+unset brew_prefix
 
 complete -C aws_completer aws

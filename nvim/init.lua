8e7d952d4f8417da41386b226babf327888fe771
Dmitry Motylev <dmitry@redsift.io>
2022-11-15 18:36:12 +0000

    some tweaks

--- nvim/init.lua
+++ nvim/init.lua
@@ -309,7 +309,7 @@ require('navigator').setup({})
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 --[=[
 -- LSP settings
@@ -431,4 +431,3 @@ cmp.setup({
     { name = 'luasnip' },
   },
 })
-

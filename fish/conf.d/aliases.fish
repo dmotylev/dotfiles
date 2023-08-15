c3d9d604916a45e6e08811f2ef41760d9e33f7e0
Dmitry Motylev <dmitry@redsift.io>
2023-08-15 19:42:22 +0100

    fish fry

--- fish/conf.d/aliases.fish
+++ fish/conf.d/aliases.fish
@@ -1,4 +1,3 @@
 # support oldschool autojump user
 alias j  z
 alias jj zz
-alias vim nvim
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -13,31 +13,33 @@ function fish_prompt --description 'Write out the prompt'
     set -q fish_prompt_pwd_dir_length
     or set -lx fish_prompt_pwd_dir_length 0
 
-    set -g __fish_git_prompt_showupstream informative
-    #set -g __fish_git_prompt_show_informative_status true
-    set -g __fish_git_prompt_showcolorhints true
-    set -g __fish_git_prompt_showuntrackedfiles true
-    set -g __fish_git_prompt_describe_style describe
-    set -g __fish_git_prompt_color_prefix brblack
-    set -g __fish_git_prompt_color_suffix brblack
-    set -g __fish_git_prompt_color brblack
-    set -g __fish_git_prompt_color_cleanstate blue
-    set -g __fish_git_prompt_color_merging yellow
+    set -g __fish_git_prompt_show_informative_status true
+    set -g __fish_git_prompt_use_informative_chars true
+
     set -g __fish_git_prompt_char_stagedstate '•'
     set -g __fish_git_prompt_color_stagedstate yellow
 
-    set -g __fish_git_prompt_showdirtystate true
     set -g __fish_git_prompt_char_dirtystate '◦'
     set -g __fish_git_prompt_color_dirtystate red
     
     set -g __fish_git_prompt_char_untrackedfiles '‥'
     set -g __fish_git_prompt_color_untrackedfiles white
     
+    set -g __fish_git_prompt_showcleanstate 0
+    set -g __fish_git_prompt_color_cleanstate green
+
+    set -g __fish_git_prompt_showcolorhints true
+    set -g __fish_git_prompt_color brblack
     set -g __fish_git_prompt_color_branch blue
     set -g __fish_git_prompt_color_branch_dirty red
     set -g __fish_git_prompt_color_branch_staged yellow
+    set -g __fish_git_prompt_color_prefix brblack
+    set -g __fish_git_prompt_color_suffix brblack
+    set -g __fish_git_prompt_color_merging yellow
 
-    set -g __fish_git_prompt_upstream true
+
+    set -g __fish_git_prompt_showupstream informative
+    set -g __fish_git_prompt_describe_style default
 
 
 
@@ -56,6 +58,6 @@ function fish_prompt --description 'Write out the prompt'
         echo -s $status_color "... exit code $last_status"
     end
 
-    echo -s $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    echo -s $cwd_color (prompt_pwd) $vcs_color (fish_git_prompt) $normal ' ' $prompt_status
     echo -n -s $status_color $suffix ' ' $normal
 end
